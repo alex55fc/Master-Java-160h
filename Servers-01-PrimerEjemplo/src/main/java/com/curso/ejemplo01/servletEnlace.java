985e32b7520595adff6b9f4fc8603b670e80b29f
Alexander Fuela <ALEXANDERPAUL.FUELA@VIEWNEXT.COM>
2024-05-02 10:43:29 +0200

    Exercise with server, example Sending data through an HTML link to a servlet where the data is received by a doGet method.

---
+++ Servers-01-PrimerEjemplo/src/main/java/com/curso/ejemplo01/servletEnlace.java
@@ -0,0 +1,34 @@
+package com.curso.ejemplo01;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class servletEnlace
+ */
+public class servletEnlace extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = request.getParameter("param1");
+		String edadToInt = request.getParameter("param2");
+		int edad = Integer.parseInt(edadToInt);
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("</body>"
+				+ " <h2>Estos datos son pasados por un enlace HTML: </h2><br>"
+				+ "La edad del usuario es " + edad 
+				+ "	y su nombre es "+ nombre
+				+ "</html>");
+		out.close();
+
+	}
+
+
+}
