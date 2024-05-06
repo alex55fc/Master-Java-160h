c613c98b3d30c1db4467897f046ef6f002544674
Alexander Fuela <ALEXANDERPAUL.FUELA@VIEWNEXT.COM>
2024-05-06 09:37:45 +0200

    Error for mapping ListaCursos.java solved, modifying web.xml root fot ListaCursos.java is the solution

--- Servers-01-PrimerEjemplo/src/main/java/com/curso/ejerciciologin/ListaCursos.java
+++ Servers-01-PrimerEjemplo/src/main/java/com/curso/ejerciciologin/ListaCursos.java
@@ -1,15 +1,14 @@
 package com.curso.ejerciciologin;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 
 /**
  * Servlet implementation class ListaCursos
