a1747453cfd724eb7964ed29975c78419175f61a
Alexander Fuela <ALEXANDERPAUL.FUELA@VIEWNEXT.COM>
2024-05-16 17:01:17 +0200

    Ejercicio crear base de datos desde eclipse con unos scripts en sql y hacer testing sobre el ejercicio. Creamos un proyecto sobre Libros y Categorias (el mismo que el ejercicio anterior) con las misma clases y todo pero en este caso cambiamos el persistence.xml ya que esta vez haremos que se creen las tablas e inserts desde el Proyecto de java. Ejecutaremos el Generar.java y este llamara al META-INF borrar, crear e insertar.sql que hara eso sobre la base de datos “bibliotec3” que creamos VACIA en MySQLWorkbech.El persistence.xml tiene una estructura distinta ya que creara las tablas y todo en la base de datos.

---
+++ RelacionesJPA-Test/src/test/java/com/curso/java/LibroTest.java
@@ -0,0 +1,61 @@
+package com.curso.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.relaciones.dominio.Libro;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class LibroTest {
+	EntityManagerFactory emf; //Arrange(organizar)  Act(ejecutar)  Assert(aseverar)
+	EntityManager em;
+	
+	@BeforeEach
+	void iniciar() {
+		emf = Persistence.createEntityManagerFactory("bibliotec3");
+		em = emf.createEntityManager();
+	}
+	
+	@Test
+	public void entityManagerFactoryTest() {
+		assertNotNull(emf);
+	}
+	
+	@Test
+	public void entityManagerTest() {
+		assertNotNull(em);
+	}
+	
+	@Test
+	public void seleccionarLibroTest() {
+		Libro libro = em.find(Libro.class, "1AB");
+		assertEquals("pepito", libro.getAutor());
+	}
+	
+	@Test
+	public void insertarLibroTest() {
+		em.getTransaction().begin();
+		Libro libro = new Libro("ZZZ","Que sueño", "Dormilon", 40);
+		em.persist(libro);
+		em.getTransaction().commit();
+		
+		Libro libroInsertado = em.find(Libro.class, "ZZZ");
+		assertEquals("ZZZ", libroInsertado.getIsbn());
+	}
+	@Test
+	public void borrarLibroTest() {
+		Libro libro = em.find(Libro.class, "1AB");
+		em.getTransaction().begin();
+		em.remove(libro);
+		em.getTransaction().commit();
+		
+		Libro noLibro = em.find(Libro.class, "1AB");
+		assertNull(noLibro);
+	}
+
+}
