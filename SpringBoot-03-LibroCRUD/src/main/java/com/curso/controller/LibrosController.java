a6e2d00fa0ad57d3006d05fba454bd51e82ffc45
Alexander Fuela <ALEXANDERPAUL.FUELA@VIEWNEXT.COM>
2024-06-06 16:45:40 +0200

    #SBoot3 added SpringDoc dependency from Maven Repository

--- SpringBoot-03-LibroCRUD/src/main/java/com/curso/controller/LibrosController.java
+++ SpringBoot-03-LibroCRUD/src/main/java/com/curso/controller/LibrosController.java
@@ -15,11 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.curso.model.Libro;
 import com.curso.service.LibrosService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 public class LibrosController {
 	@Autowired
 	LibrosService service;
 	
+
 	@GetMapping(value="libros", produces=MediaType.APPLICATION_JSON_VALUE)
 	 public List<Libro> libros(){
 		 return service.libros();
@@ -28,6 +35,21 @@ public class LibrosController {
 	 Libro buscarLibro(@PathVariable int isbn) {
 		 return service.buscarLibro(isbn);
 	 }
+	
+	//para el swagger
+    @Operation(
+            summary="Crea un nuevo Libro.",
+            description="Crea un nuevo Libro a partir de los datos recibidos"
+            )
+
+	@ApiResponses({
+	    @ApiResponse(responseCode ="200", description ="OK", content = 
+	    {@Content(mediaType ="application/JSON", schema=
+	    @Schema(implementation=Libro.class))
+	    }),
+	    @ApiResponse(responseCode ="401", description="invalido lo que sea"),
+	})
+	
 	@PostMapping(value="libros", consumes=MediaType.APPLICATION_JSON_VALUE)
 	 void altaLibro(@RequestBody Libro libro) {
 		 service.altaLibro(libro);
